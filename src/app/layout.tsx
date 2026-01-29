78fa6727987a5784cf99f4733d2678b49a9731d9
Tony Kan <tonykan@caltech.edu>
2026-01-28 19:47:35 -0800

    feat(layout): Update metadata to use siteConfig for dynamic title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Caltech MSC",
-  description: "Materials and Process Simulation Center at Caltech",
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
 };
 
 export default function RootLayout({
