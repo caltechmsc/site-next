947fa72108002361c94d3050957794a8a981c368
Tony Kan <tonykan@caltech.edu>
2026-01-29 12:02:17 -0800

    chore(project): Add image remote patterns for external sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
