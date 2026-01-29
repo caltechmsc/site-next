f394792fa1969bc900184353d8148284608dc285
Tony Kan <tonykan@caltech.edu>
2026-01-28 19:06:53 -0800

    feat(config): Add configuration exports for environment and site settings

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Configuration Exports
+ */
+
+export { env, type Env } from "./env";
+export {
+  siteConfig,
+  mainNav,
+  adminNav,
+  features,
+  seoDefaults,
+  type NavItem,
+} from "./site";
