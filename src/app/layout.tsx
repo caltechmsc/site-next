16f02cd260be3c053e56776442c12b18335b1c10
Tony Kan <tonykan@caltech.edu>
2026-01-29 11:28:36 -0800

    feat(layout): Update metadataBase to use siteConfig URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
   title: {
     default: siteConfig.name,
     template: `%s | ${siteConfig.name}`,
