0a00eb54b3ec11bcbb989fac21cf68d914beff1d
Tony Kan <tonykan@caltech.edu>
2026-01-30 02:20:34 -0800

    feat(publication): Add index file to export publication components

---
+++ src/components/publication/index.ts
@@ -0,0 +1,4 @@
+export { PublicationCard } from "./publication-card";
+export { PublicationList } from "./publication-list";
+export { PublicationFilters } from "./publication-filters";
+export { CitationBox } from "./citation-box";
