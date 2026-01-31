8adc0280c348511a6712512346d2935fac1f6cf2
Tony Kan <tonykan@caltech.edu>
2026-01-30 16:59:00 -0800

    chore(project): Remove unused remote image patterns from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,19 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "user-images.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
+        hostname: "picsum.photos",
       },
     ],
   },
