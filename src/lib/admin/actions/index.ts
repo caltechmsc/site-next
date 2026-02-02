0e91d75315fc36634e02f889c7bc5b525af3f3ec
Tony Kan <tonykan@caltech.edu>
2026-02-01 20:19:00 -0800

    feat(admin): Add index file for admin action types and utility functions

---
+++ src/lib/admin/actions/index.ts
@@ -0,0 +1,35 @@
+export type {
+  ActionSuccess,
+  ActionError,
+  ActionValidationError,
+  ActionResult,
+  FieldErrors,
+  MemberFormData,
+  CategoryFormData,
+  ResearchAreaFormData,
+  CollaboratorFormData,
+  PhotoFormData,
+  PhotoUploadData,
+  AdminRole,
+  AdminCreateFormData,
+  AdminUpdateFormData,
+  ChangePasswordData,
+  ReorderItem,
+  ReorderInput,
+} from "./types";
+
+export {
+  hasFieldErrors,
+  success,
+  successVoid,
+  error,
+  validationError,
+} from "./types";
+
+export {
+  createSafeAction,
+  createAction,
+  ActionError as ActionErrorClass,
+  assertAction,
+  ensureExists,
+} from "./utils";
