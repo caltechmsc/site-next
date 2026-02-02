f781581643d2fc83b73ada947abf3031fd637eeb
Tony Kan <tonykan@caltech.edu>
2026-02-01 17:51:42 -0800

    feat(admin): Create layout index file to export admin panel components

---
+++ src/components/admin/layout/index.ts
@@ -0,0 +1,12 @@
+export {
+  SIDEBAR_WIDTH,
+  SIDEBAR_COLLAPSED_WIDTH,
+  SIDEBAR_COLLAPSED_KEY,
+} from "./constants";
+
+export { Sidebar } from "./sidebar";
+export { SidebarHeader } from "./sidebar-header";
+export { SidebarNav } from "./sidebar-nav";
+export { SidebarFooter } from "./sidebar-footer";
+export { MobileNav } from "./mobile-nav";
+export { TopBar } from "./top-bar";
