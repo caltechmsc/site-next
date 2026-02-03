eaa047c2f271f2fe893ed31914c41036c819a16c
Tony Kan <tonykan@caltech.edu>
2026-02-03 00:49:38 -0800

    chore(project): Add experimental server actions with body size limit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "15mb",
+    },
+  },
+
   images: {
     remotePatterns: [
       {
