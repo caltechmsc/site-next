8fff5780b960e6808e7ff324701487cbf9230833
Tony Kan <tonykan@caltech.edu>
2026-02-04 08:42:55 -0800

    refactor(publication): Remove PublicationList component and associated logic

--- src/components/publication/index.ts
+++ src/components/publication/index.ts
@@ -1,4 +1,3 @@
 export { PublicationCard } from "./publication-card";
-export { PublicationList } from "./publication-list";
 export { PublicationFilters } from "./publication-filters";
 export { CitationBox } from "./citation-box";
