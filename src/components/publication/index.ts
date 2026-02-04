123e2aad6091a302171c538380b7281899f192d9
Tony Kan <tonykan@caltech.edu>
2026-02-04 08:43:23 -0800

    feat(publication): Re-export PublicationsByYear and PublicationsWithFilters components

--- src/components/publication/index.ts
+++ src/components/publication/index.ts
@@ -1,3 +1,5 @@
 export { PublicationCard } from "./publication-card";
 export { PublicationFilters } from "./publication-filters";
 export { CitationBox } from "./citation-box";
+export { PublicationsByYear } from "./publications-by-year";
+export { PublicationsWithFilters } from "./publications-with-filters";
