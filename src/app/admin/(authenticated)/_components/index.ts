ebeb4cf262a95d0faec6239e3fe778a54c299e91
Tony Kan <tonykan@caltech.edu>
2026-02-05 15:46:57 -0800

    feat(admin): Add index file to export admin components for easier imports

---
+++ src/app/admin/(authenticated)/_components/index.ts
@@ -0,0 +1,6 @@
+export {
+  PublicationChart,
+  type PublicationChartProps,
+} from "./publication-chart";
+export { RecentActivity, type RecentActivityProps } from "./recent-activity";
+export { StatsGrid, type StatsGridProps } from "./stats-grid";
