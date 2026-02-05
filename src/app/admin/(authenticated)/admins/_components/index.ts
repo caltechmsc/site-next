13c66abf18c020745d4ca8b1594393df62f8474d
Tony Kan <tonykan@caltech.edu>
2026-02-05 13:37:26 -0800

    feat(admin): Create index file for admin components export

---
+++ src/app/admin/(authenticated)/admins/_components/index.ts
@@ -0,0 +1,11 @@
+export { AdminList, type AdminListProps } from "./admin-list";
+export { AdminRow, type AdminRowProps } from "./admin-row";
+export {
+  AdminFormDialog,
+  type AdminFormDialogProps,
+} from "./admin-form-dialog";
+export {
+  ChangePasswordDialog,
+  type ChangePasswordDialogProps,
+} from "./change-password-dialog";
+export { AccountView, type AccountViewProps } from "./account-view";
