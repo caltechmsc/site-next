d3361deedaf91a921c1e80fb1deb4ecec8105ddd
Tony Kan <tonykan@caltech.edu>
2026-02-05 01:53:17 -0800

    feat(admin): Create index file for exporting collaborator components

---
+++ src/app/admin/(authenticated)/collaborators/_components/index.ts
@@ -0,0 +1,10 @@
+export {
+  CollaboratorList,
+  type CollaboratorListProps,
+} from "./collaborator-list";
+export { CollaboratorRow, type CollaboratorRowProps } from "./collaborator-row";
+export {
+  CollaboratorFormDialog,
+  type CollaboratorFormDialogProps,
+} from "./collaborator-form-dialog";
+export { LocationPicker, type LocationPickerProps } from "./location-picker";
