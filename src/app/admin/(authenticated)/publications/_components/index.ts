71c735b4c52d548c497a2e7c9fe1ccada10873f9
Tony Kan <tonykan@caltech.edu>
2026-02-06 13:32:48 -0800

    feat(admin): Create index file for publication components export

---
+++ src/app/admin/(authenticated)/publications/_components/index.ts
@@ -0,0 +1,6 @@
+export { PublicationList, type PublicationListProps } from "./publication-list";
+export { PublicationRow, type PublicationRowProps } from "./publication-row";
+export {
+  PublicationFormDialog,
+  type PublicationFormDialogProps,
+} from "./publication-form-dialog";
